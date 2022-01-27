a85f77a2287af74beb0f1debe4efeb7411f678fc
MoonLight-Sherry <3397446353@qq.com>
2022-01-27 21:19:01 +0800

    Update Memory.h

---
+++ lib/Memory/Memory.h
@@ -0,0 +1,25 @@
+#ifndef __TIMER_MEMORY__
+#define __TIMER_MEMORY__
+
+#include <Arduino.h>
+#include <EEPROM.h>
+
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t u8;
+
+class Memory
+{
+    u8 data, p;
+    u16 writeAddr, readAddr;
+
+public:
+    void writeData(u8 data);
+    void writeData(u8 data, u8 _addr);
+    void writeData(u16 _addr, u16 len, u8 *_p);
+    u8 readData(void);
+    u8 readData(u16 _addr);
+    void readData(u16 _addr, u16 len, u8 *_p);
+};
+
+#endif
